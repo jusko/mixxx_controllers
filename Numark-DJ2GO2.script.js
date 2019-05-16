c7975bb3ee9c745ca75f62cb5120c979e03043e3
Justin Kourie <justin.kourie+github@gmail.com>
2019-05-16 07:15:34 +0200

    Implements press/release for sync buttons

--- Numark-DJ2GO2.script.js
+++ Numark-DJ2GO2.script.js
@@ -1,13 +1,19 @@
 var NumarkDJ2GO2 = {};
 
+// Init
 NumarkDJ2GO2.init = function (id, debug) {
 };
 
+// Shutdown
 NumarkDJ2GO2.shutdown = function (id, debug) {
   midi.sendShortMsg(0x80, 0x1B, 0x01);
   midi.sendShortMsg(0x81, 0x1B, 0x01);
 };
 
+NumarkDJ2GO2.Deck = function (channel) {
+};
+
+// Headphones
 NumarkDJ2GO2.headphonesOn = function(channel, control, value, status, group) {
   midi.sendShortMsg(0x90 + channel, 0x1B, 0x01);
   engine.setValue(group, 'pfl', 1);
@@ -16,4 +22,15 @@ NumarkDJ2GO2.headphonesOn = function(channel, control, value, status, group) {
 NumarkDJ2GO2.headphonesOff = function(channel, control, value, status, group) {
   midi.sendShortMsg(0x80 + channel, 0x1B, 0x01);
   engine.setValue(group, 'pfl', 0);
+}
+
+// Sync
+NumarkDJ2GO2.syncPressed = function(channel, control, value, status, group) {
+  midi.sendShortMsg(0x90 + channel, 0x02, 0x01);
+  engine.setValue(group, 'sync_enabled', 1);
+}
+
+NumarkDJ2GO2.syncReleased = function(channel, control, value, status, group) {
+  midi.sendShortMsg(0x90 + channel, 0x02, 0x00);
+  engine.setValue(group, 'sync_enabled', 0);
 }
