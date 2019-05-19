6018ef5ac05e13a31e109210febc59b27a5ddff5
Justin Kourie <justin.kourie+github@gmail.com>
2019-05-19 07:57:21 +0200

    Substantial refactor to implement hot swapping decks mapped to different controls
    Adds the following object hierarchy:
    
    - DeckBase: encapsulates controls which don't change when a deck is swapped
    - StandardDeck: implements the standard controls as would be the case in Serato
    - EqualizerDeck: remaps the jog wheel and knobs to the low, mid and high eq gain effects
    - MultiDeck: a container to toggle between a StandardDeck and EqualizerDeck instance

--- Numark-DJ2GO2.script.js
+++ Numark-DJ2GO2.script.js
@@ -13,22 +13,29 @@ NumarkDJ2GO2.init = function (id, debug) {
     return (status & 0xF0) === 0x90;
   }
 
-  NumarkDJ2GO2.shiftMode = false;
+  /**
+   * For whatever reason this refuses to work when the EqGainKnob (or even just
+   * plain Pot components) connect for the first time in the Deck constructors
+   * (but only in init(), not when swapping a deck for the first time). Remove
+   * after implementing serato_sysex
+  */
+  engine.softTakeover('[Master]', 'gain', true);
+  engine.softTakeover('[Master]', 'headGain', true);
+  engine.softTakeover('[Channel1]', 'pregain', true);
+  engine.softTakeover('[Channel2]', 'pregain', true);
 
-  NumarkDJ2GO2.setSuperGain = function(on) {
-    if (on) {
-      NumarkDJ2GO2.master = new NumarkDJ2GO2.SuperGainMaster();
-      NumarkDJ2GO2.leftDeck = new NumarkDJ2GO2.SuperGainDeck(0);
-      NumarkDJ2GO2.rightDeck = new NumarkDJ2GO2.SuperGainDeck(1);
-    }
-    else {
-      NumarkDJ2GO2.master = new NumarkDJ2GO2.Master();
-      NumarkDJ2GO2.leftDeck = new NumarkDJ2GO2.Deck(0);
-      NumarkDJ2GO2.rightDeck = new NumarkDJ2GO2.Deck(1);
-    }
-  };
-  NumarkDJ2GO2.setSuperGain(false);
+  NumarkDJ2GO2.shiftMode = false;
+  NumarkDJ2GO2.decks = [
+    new NumarkDJ2GO2.MultiDeck(0),
+    new NumarkDJ2GO2.MultiDeck(1)
+  ];
+  NumarkDJ2GO2.setDecks();
 };
+
+NumarkDJ2GO2.setDecks = function() {
+  NumarkDJ2GO2.leftDeck = NumarkDJ2GO2.decks[0].getDeck();
+  NumarkDJ2GO2.rightDeck = NumarkDJ2GO2.decks[1].getDeck();
+}
 
 /**
  * Shutdown
@@ -54,76 +61,17 @@ NumarkDJ2GO2.shiftModeOff = function () {
   NumarkDJ2GO2.shiftMode = false;
 };
 
-/**
- * Enter/exit "super gain" mode if load buttons pressed in shift mode
- */
-NumarkDJ2GO2.enableSuperGain = function() {
-  if (NumarkDJ2GO2.shiftMode) {
-    NumarkDJ2GO2.setSuperGain(true);
-  }
-}
-
-NumarkDJ2GO2.disableSuperGain = function() {
-  if (NumarkDJ2GO2.shiftMode) {
-    NumarkDJ2GO2.setSuperGain(false);
-  }
-}
 
 /**
- * Regular master channel controls
+ * Core deck controls
  */
-NumarkDJ2GO2.Master = function () {
-  components.ComponentContainer.call(this)
-
-  this.level = new components.Pot({
-    midi: [0xBF, 0X0A],
-    group: '[Master]',
-    inKey: 'gain'
-  });
-
-  this.cueLevel = new components.Pot({
-    midi: [0xBF, 0X0C],
-    group: '[Master]',
-    inKey: 'headGain'
-  });
-};
-NumarkDJ2GO2.Master.prototype = Object.create(components.ComponentContainer.prototype)
-
-/*
- * Master channel controls transforming master and cue level knobs to mid gain
- * equalizer knobs for both channels
- */
-NumarkDJ2GO2.SuperGainMaster = function () {
-  components.ComponentContainer.call(this)
-
-  this.level = new NumarkDJ2GO2.EqGainKnob(0, {
-    midi: [0xBF, 0X0A],
-    inKey: 'parameter2'
-  });
-
-  this.cueLevel = new NumarkDJ2GO2.EqGainKnob(1, {
-    midi: [0xBF, 0X0C],
-    inKey: 'parameter2'
-  });
-};
-NumarkDJ2GO2.SuperGainMaster.prototype = Object.create(components.ComponentContainer.prototype)
-
-/**
- * A deck with the standard controller features
- */
-NumarkDJ2GO2.Deck = function (channel) {
+NumarkDJ2GO2.DeckBase = function (channel) {
   components.Deck.call(this, [channel + 1]);
 
   this.playButton = new components.PlayButton([0x90 + channel, 0x00]);
   this.cueButton = new components.CueButton([0x90 + channel, 0x01]);
   this.syncButton = new components.SyncButton([0x90 + channel, 0x02]);
-
-  this.jogWheel = new NumarkDJ2GO2.JogWheel(channel);
-
-  this.gainLevel = new components.Pot({
-    midi: [0xB0 + channel, 0X16],
-    inKey: 'pregain'
-  });
+  this.loadButton = new NumarkDJ2GO2.LoadButton([0x9F, 0x02 - channel]);
 
   this.reconnectComponents(function (component) {
     if (component.group === undefined) {
@@ -131,22 +79,46 @@ NumarkDJ2GO2.Deck = function (channel) {
     }
   });
 };
-NumarkDJ2GO2.Deck.prototype = Object.create(components.Deck.prototype);
+NumarkDJ2GO2.DeckBase.prototype = Object.create(components.Deck.prototype);
 
 /**
- * A deck which transforms its jog wheel and gain level knob to low and hi gain
- * equalizer knobs respectively.
+ * Standard deck
  */
-NumarkDJ2GO2.SuperGainDeck = function (channel) {
-  NumarkDJ2GO2.Deck.call(this, channel);
+NumarkDJ2GO2.StandardDeck = function (channel) {
+  this.jogWheel = new NumarkDJ2GO2.JogWheel(channel);
 
+  this.knob1 = new components.Pot({
+    midi: [0xBF, (channel === 0 ? 0x0A : 0x0C)],
+    inKey: channel == 0 ? 'gain' : 'headGain',
+    group: '[Master]'
+  });
+
+  this.knob2 = new components.Pot({
+    midi: [0xB0 + channel, 0x16],
+    inKey: 'pregain'
+  });
+  NumarkDJ2GO2.DeckBase.call(this, channel);
+};
+NumarkDJ2GO2.StandardDeck.prototype = Object.create(NumarkDJ2GO2.DeckBase.prototype);
+
+/**
+ * Equalizer deck
+ */
+NumarkDJ2GO2.EqualizerDeck = function (channel) {
   this.jogWheel = new NumarkDJ2GO2.JogWheelGain(channel, 'parameter1');
-  this.gainLevel = new NumarkDJ2GO2.EqGainKnob(channel, {
-    midi: [0xB0 + channel, 0X16],
+
+  this.knob1 = new NumarkDJ2GO2.EqGainKnob(channel, {
+    midi: [0xBF, (channel === 0 ? 0x0A : 0x0C)],
+    inKey: 'parameter2'
+  });
+
+  this.knob2 = new NumarkDJ2GO2.EqGainKnob(channel, {
+    midi: [0xB0 + channel, 0x16],
     inKey: 'parameter3'
   });
+  NumarkDJ2GO2.DeckBase.call(this, channel);
 };
-NumarkDJ2GO2.SuperGainDeck.prototype = Object.create(NumarkDJ2GO2.Deck.prototype);
+NumarkDJ2GO2.EqualizerDeck.prototype = Object.create(NumarkDJ2GO2.DeckBase.prototype);
 
 /**
  * Headphones/PFL Events 
@@ -165,18 +137,16 @@ NumarkDJ2GO2.headphonesOff = function(channel, control, value, status, group) {
 };
 
 /**
- * Custom Components
- */
-
-/**
  * Standard jog wheel
  */
 NumarkDJ2GO2.JogWheel = function (channel) {
   components.Encoder.call(this);
   this.midi = [0xB0 + channel, 0x06];
   this.group = '[Channel' + (channel + 1) + ']';
-  this.inKey = 'playposition';
-  this.input = function(channel, control, value) {
+}
+NumarkDJ2GO2.JogWheel.prototype = new components.Encoder({
+  inKey: 'playposition',
+  input: function(channel, control, value) {
     var tick = NumarkDJ2GO2.shiftMode ? 0.00025  : 0.01;
     if (value === 0x01) {
       this.inSetParameter(this.inGetParameter() + tick);
@@ -184,28 +154,29 @@ NumarkDJ2GO2.JogWheel = function (channel) {
     else if (value === 0x7F) {
       this.inSetParameter(this.inGetParameter() - tick);
     }
-  };
-}
-NumarkDJ2GO2.JogWheel.prototype = Object.create(components.Encoder.prototype);
+  }
+});
 
 /**
  * Jog wheel mapped to a gain level
  */
 NumarkDJ2GO2.JogWheelGain = function (channel, gain) {
-  NumarkDJ2GO2.JogWheel.call(this, channel);
+  components.Encoder.call(this);
 
+  this.midi = [0xB0 + channel, 0x06];
   this.group = '[EqualizerRack1_[Channel' + (channel + 1) + ']_Effect1]';
   this.inKey = gain;
-  this.input = function(channel, control, value) {
+}
+NumarkDJ2GO2.JogWheelGain.prototype = new components.Encoder({
+  input: function(channel, control, value) {
     if (value === 0x01) {
       this.inSetParameter(this.inGetParameter() + 0.005);
     }
     else if (value === 0x7F) {
       this.inSetParameter(this.inGetParameter() - 0.005);
     }
-  };
-}
-NumarkDJ2GO2.JogWheelGain.prototype = Object.create(NumarkDJ2GO2.JogWheel.prototype);
+  }
+});
 
 /**
  * Equalizer gain level knob
@@ -215,4 +186,58 @@ NumarkDJ2GO2.EqGainKnob = function (channel, options) {
 
   this.group = '[EqualizerRack1_[Channel' + (channel + 1) + ']_Effect1]';
 }
-NumarkDJ2GO2.EqGainKnob.prototype = Object.create(components.Pot.prototype);
+/**
+ * These are override because there's some funky code going on in Pot's default
+ * connect methods (go look, it checks a variable, this.relative, which doesn't 
+ * even exist anywhere). This works.
+ */
+NumarkDJ2GO2.EqGainKnob.prototype = new components.Pot({
+  connect: function() {
+    engine.softTakeover(this.group, this.inKey, true);
+  },
+  disconnect: function() {
+    engine.softTakeoverIgnoreNextValue(this.group, this.inKey);
+  }
+});
+
+/**
+ * Load button
+ */
+NumarkDJ2GO2.LoadButton = function(channel, midi) {
+  components.Button.call(this);
+  this.channel = channel;
+  this.midi = midi;
+  this.group = '[Channel16]';
+}
+NumarkDJ2GO2.LoadButton.prototype = new components.Button({
+  input: function(channel, control) {
+    if (NumarkDJ2GO2.shiftMode) {
+      NumarkDJ2GO2.decks[control - 0x02].toggle();
+      NumarkDJ2GO2.setDecks();
+    }
+  }
+});
+
+/**
+ * Simple deck container to track toggling between single instances of
+ * StandardDeck and EqualizerDeck
+ */
+NumarkDJ2GO2.MultiDeck = function(channel) {
+  this.currentDeck = 0;
+  this.decks = [
+    new NumarkDJ2GO2.StandardDeck(channel),
+    new NumarkDJ2GO2.EqualizerDeck(channel)
+  ];
+};
+NumarkDJ2GO2.MultiDeck.prototype = new Object({
+  toggle: function() {
+    this.decks[this.currentDeck].forEachComponent(function(component) {
+      component.disconnect();
+    });
+    this.currentDeck = this.currentDeck === 0 ? 1 : 0;
+    this.decks[this.currentDeck].reconnectComponents();
+  },
+  getDeck: function() {
+    return this.decks[this.currentDeck];
+  }
+});
